8c93c9421657840a049106cfc18d02bb3b6adbe8
wveik <kataev.06@gmail.com>
2019-05-21 23:14:24 +0300

    Add Common project for DTO

---
+++ Common/DTO/PlayerDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.DTO
+{
+	public class PlayerDto
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Date { get; set; }
+		public bool IsInGame { get; set; }
+		public bool IsHealthy { get; set; }
+		public int Salary { get; set; }
+	}
+}
